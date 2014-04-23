75c3e545c3f3e239b619714680417f42b3f1e878
Pierre-Baptiste Varlet <pierre-baptiste.varlet@univ-reims.fr>
2014-04-23 16:40:25 +0200

    Adding Component/GoogleAPIComponent.php and GoogleAPIAppController.php files Editing Config/core.php.default

---
+++ Controller/Component/GoogleAPIComponent.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Licensed under The GPL V3 License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @package       GoogleAPI.Controller.Component
+ * @license       http://opensource.org/licenses/GPL-3.0 GPL V3 License
+ */
+
+App::uses('Component', 'Controller');
+
+class GoogleAPIComponent extends Component
+{
+}
---
+++ Controller/GoogleAPIAppController.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Licensed under The GPL V3 License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @package       GoogleAPI.Controller
+ * @license       http://opensource.org/licenses/GPL-3.0 GPL V3 License
+ */
+
+App::uses('Controller', 'Controller');
+App::uses('AppController', 'Controller');
+
+class GoogleAPIAppController extends AppController
+{
+}
